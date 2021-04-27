8aa8a5a992d4d3603832a2b8d10bd757e1b100d4
PaulDenis <d.paul@outlook.it>
2021-04-27 16:35:41 +0200

    Progetto inziato, richiesta dati completa

---
+++ script/script.js
@@ -0,0 +1,6 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+var chilometri = prompt ("Quanti chilometri desideri percorrere?");
+console.log(chilometri);
+var eta = prompt ("Quanti anni hai?")
+console.log(eta);
